f84053e903865066159d9cfb718054d1cab50f09
Volha Vybarnaya <olyaviborn@gmail.com>
2025-01-11 15:56:51 +0300

    commit 11/01

--- mysite/recipesapp/admin.py
+++ mysite/recipesapp/admin.py
@@ -4,5 +4,6 @@ from recipesapp.models import Recipe
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = 'id', 'title', 'description'
+    list_display = 'id', 'title', 'description', "created_at", "updated_at"
     list_display_links = 'id', 'title'
+    search_fields = ['title', 'description']
--- mysite/recipesapp/models.py
+++ mysite/recipesapp/models.py
@@ -1,16 +1,28 @@
-from django.db import models
-
+from django.contrib.auth.models import User
 from django.db import models
 
 class Recipe(models.Model):
 
     class Meta:
         verbose_name = "Recipe"
+        # ordering = ['-created_at']  # Sort by most recently created first
+        ordering = ['-title', 'created_at']
 
     title = models.CharField(max_length=150)
     description = models.TextField()
     steps_of_cooking = models.TextField()
     time_for_cooking = models.IntegerField(default=15)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    @property
+    def description_short(self):
+        if len(self.description) < 25:
+            return self.description
+        else:
+            return self.description[:25] + "..."
 
     def __str__(self):
         return self.title
