ea3437e1b552941f81854de0a28f2d36cd0fa925
Volha Vybarnaya <olyaviborn@gmail.com>
2025-02-07 12:05:16 +0300

    configure for deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -18,8 +18,8 @@ from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# DATABASE_DIR = BASE_DIR / "database"
-# DATABASE_DIR.mkdir(exist_ok=True)
+DATABASE_DIR = BASE_DIR / "database"
+DATABASE_DIR.mkdir(exist_ok=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -34,7 +34,7 @@ DEBUG = getenv("DJANGO_DEBUG", "1") == "1"
 ALLOWED_HOSTS = [
     "0.0.0.0",
     "127.0.0.1",
-]
+] + getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 INTERNAL_IPS =[
     "127.0.0.1",
@@ -103,8 +103,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME': DATABASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DATABASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -169,6 +169,31 @@ LOGFILE_NAME = BASE_DIR / 'log.txt'
 LOGFILE_SIZE = 1 * 1024 * 1024
 LOGFILE_COUNT = 3
 LOGLEVEL = getenv("DJANGO_LOGLEVEL", "info").upper()
+
+# logging.config.dictConfig(
+#     {
+#         'version': 1,
+#         'disable_existing_loggers': False,
+#         'formatters': {
+#            'console': {
+#                'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+#             },
+#         },
+#         'handlers': {
+#             'console': {
+#                 'class': 'logging.StreamHandler',
+#                 'formatter': 'console',
+#             },
+#         },
+#         "loggers":{
+#             "level": LOGLEVEL,
+#             "handlers": {
+#                 "console",
+#             },
+#         },
+#     }
+# )
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -191,7 +216,7 @@ LOGGING = {
         },
     },
     'root': {
-            'level': 'INFO',
+            'level': LOGLEVEL,
             'handlers': ['console', 'logfile'],
     },
 }
